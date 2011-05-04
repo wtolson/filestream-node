e8e6529197b35b1c6d724c0ac336514fe95d0252
William Trevor Olson <wtolson@gmail.com>
2011-05-04 10:29:45 -0700

    Created task queue so that they will not screw with each other.

--- filestream.js
+++ filestream.js
@@ -24,6 +24,27 @@
     var bufferSize = options.bufferSize || defaults.bufferSize;
     var buffer = new Buffer(bufferSize);
 
+    var queue = [];
+    var running = false;
+
+    function addTask(fn, that, args) {
+      if (running) {
+        queue.push({fn: fn, that: that, args: args});
+      } else {
+        running = true;
+        fn.apply(that, args);
+      }
+    };
+
+    function doNext() {
+      if (queue.length) {
+        var task = queue.shift();
+        task.fn.apply(task.that, task.args);
+      } else {
+        running = false;
+      }      
+    };
+
     function fillBuffer(cb) {
       fs.read(file, buffer, 0, bufferSize, filePos, function(err, bytesRead) {
         filePos += bytesRead;
@@ -37,102 +58,108 @@
       });
     };
 
+    function do_read(n, cb) {
+      if (!stream.readable) {
+        doNext();
+        throw new Error("Stream is not readable");
+      }
+      var data = [];
+      if (typeof cb === 'undefined') {
+        cb = n;
+        n = null;
+      }
+
+      if (n < 0) {
+        n = null;
+      }
+
+      function finish(err) {
+        if (err) {
+          return cb(err), doNext();
+        } else {
+          return cb(null, data.join('')), doNext();
+        }
+      };
+
+
+      (function grabBuffer(err) {
+        if (err) {
+          return finish(err);
+        }
+
+        if (bufferStart == bufferEnd) {
+          if (foundEOF) {
+            return finish();
+          }
+
+          return fillBuffer(grabBuffer);
+        }
+          
+        var frag = buffer.toString(stream.encoding, bufferStart, bufferEnd);
+        if (n === null || n > frag.length) {
+          data.push(frag);
+          n -= frag.length;
+          return fillBuffer(grabBuffer);
+        } else {
+          data.push(frag.substr(0,n));
+          bufferStart += n;
+          return finish();
+        }
+      })();
+    };
+
+    function do_readline(cb) {
+      if (!stream.readable) {
+        doNext();
+        throw new Error("Stream is not readable");
+      }
+
+      var line = [];
+      function finish(err) {
+        if (err) {
+          return cb(err), doNext();
+        } else {
+          return cb(null, line.join('')), doNext();
+        }
+      };
+
+      (function grabBuffer(err) {
+        if (err) {
+          return finish(err);
+        }
+
+        if (bufferStart == bufferEnd) {
+          if (foundEOF) {
+            return finish();
+          }
+
+          return fillBuffer(grabBuffer);
+        }
+
+        var frag = buffer.toString(stream.encoding, bufferStart, bufferEnd);
+        var end = frag.indexOf(stream.newlines);
+        if (end === -1) {
+          line.push(frag);
+          return fillBuffer(grabBuffer);
+        } else {
+          line.push(frag.substr(0,end));
+          bufferStart += end+1;
+          return finish();
+        }
+      })();
+    };
+
     var stream = {
       newlines: options.newlines || defaults.newlines,
 
       encoding: options.encoding || defaults.encoding,
 
       read: function(n, cb) {
-        if (!stream.readable) {
-          throw new Error("Stream is not readable");
-        }
-
-        var data = [];
-
-        if (typeof cb === 'undefined') {
-          cb = n;
-          n = null;
-        }
-
-        if (n < 0) {
-          n = null;
-        }
-
-        function finish(err) {
-          if (err) {
-            return cb(err);
-          } else {
-            return cb(null, data.join(''));
-          }
-        };
-
-
-        (function grabBuffer(err) {
-          if (err) {
-            return finish(err);
-          }
-
-          if (bufferStart == bufferEnd) {
-            if (foundEOF) {
-              return finish();
-            }
-
-            return fillBuffer(grabBuffer);
-          }
-          
-          var frag = buffer.toString(stream.encoding, bufferStart, bufferEnd);
-          if (n === null || n > frag.length) {
-            data.push(frag);
-            n -= frag.length;
-            return fillBuffer(grabBuffer);
-          } else {
-            data.push(frag.substr(0,n));
-            bufferStart += n;
-            return finish();
-          }
-        })();
-
+        addTask(do_read, this, arguments);
       },
 
       readline: function(cb) {
-        if (!stream.readable) {
-          throw new Error("Stream is not readable");
-        }
-
-        var line = [];
-        function finish(err) {
-          if (err) {
-            return cb(err);
-          } else {
-            return cb(null, line.join(''));
-          }
-        };
-
-        (function grabBuffer(err) {
-          if (err) {
-            return finish(err);
-          }
-
-          if (bufferStart == bufferEnd) {
-            if (foundEOF) {
-              return finish();
-            }
-
-            return fillBuffer(grabBuffer);
-          }
-
-          var frag = buffer.toString(stream.encoding, bufferStart, bufferEnd);
-          var end = frag.indexOf(stream.newlines);
-          if (end === -1) {
-            line.push(frag);
-            return fillBuffer(grabBuffer);
-          } else {
-            line.push(frag.substr(0,end));
-            bufferStart += end+1;
-            return finish();
-          }
-        })();
-
+        addTask(do_readline, this, arguments);
       },
 
       get readable() {
